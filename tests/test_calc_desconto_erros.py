3c220e3a17ef62b35b115a61a3a61a7bbadf8c0d
Cauã Gustavo <caua.g.silva16@aluno.senai.br>
2026-05-08 17:19:21 -0300

    MOD: Acertando testes, mudanças de relatórios e etc...

--- tests/test_calc_desconto_erros.py
+++ tests/test_calc_desconto_erros.py
@@ -3,24 +3,24 @@ from desconto.desconto import calcular_desconto
 
 def test_valores_NaN():
     with pytest.raises(TypeError, match="Erro: O valor total e o desconto devem ser numéricos"):
-        calcular_desconto("casa", None)
+        calcular_desconto("alo", None)
 
 def test_valor_total_NaN():
     with pytest.raises(TypeError, match="Erro: O valor total e o desconto devem ser numéricos"):
-        calcular_desconto('casa', 10)
+        calcular_desconto('alo', 10)
 
 def test_desconto_NaN():
     with pytest.raises(TypeError, match="Erro: O valor total e o desconto devem ser numéricos"):
-        calcular_desconto(10, 'senai')
+        calcular_desconto(10, 'alo')
 
 def test_valor_total_menor_0():
     with pytest.raises(ValueError, match="Erro: O valor do produto não pode ser negativo."):
-        calcular_desconto(-10, 25.6)
+        calcular_desconto(-3,98.2)
 
 def test_desconto_maior_100():
     with pytest.raises(ValueError, match="Erro: O desconto deve estar entre 0 e 100."):
-        calcular_desconto(105, 110)
+        calcular_desconto(11, 111)
 
 def test_desconto_menor_0():
     with pytest.raises(ValueError, match="Erro: O desconto deve estar entre 0 e 100."):
-        calcular_desconto(105, -0.1)
+        calcular_desconto(2023, -100)
